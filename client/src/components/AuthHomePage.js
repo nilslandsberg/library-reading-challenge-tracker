f2ae8c325f1f87dae653a46cd7824638587e8313
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-05-10 16:15:40 -0400

    MyReaders rendering

--- client/src/components/AuthHomePage.js
+++ client/src/components/AuthHomePage.js
@@ -2,8 +2,17 @@ import { Container, Row } from "react-bootstrap"
 import MyReaders from "../containers/MyReaders"
 import AddReaderButton from "./AddReaderButton"
 import afroBoy from '../files/avatars/4043235_afro_boy_child_kid_icon.svg';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getReadersAction } from "../features/readerSlice";
 
 const AuthHomePage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getReadersAction());
+  }, [dispatch]);
+
   return (
     <>
         <Container className="readers">
--- client/src/containers/MyReaders.js
+++ client/src/containers/MyReaders.js
@@ -1,6 +1,29 @@
+import { Card } from "react-bootstrap"
+import { useSelector } from "react-redux"
+
 const MyReaders = () => {
+  const userReaders = useSelector((state) => state.userReaders.readers)
+  console.log(userReaders)
   return (
-    <h1>My Readers</h1>
+    <>
+      {userReaders && userReaders.length > 0 ? (
+        <>
+          {userReaders.map((reader, index) => (
+            <Card className="reader-card" key={reader._id} style={{ width: '18rem' }}>
+              <Card.Img src={reader.avatar} variant="top" />
+              <Card.Body>
+                {reader.name}
+              </Card.Body>
+              <Card.Body>
+                {reader.age}
+              </Card.Body>
+            </Card>
+          ))}
+        </>
+      ) : (
+        <div></div>
+      )}
+    </>
   )
 }
 
