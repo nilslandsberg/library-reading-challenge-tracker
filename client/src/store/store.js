b39d37f622ae17741ffd65f2b7beca63832d98ea
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-05-12 21:09:11 -0400

    Book search by keyword, author, and title functional

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import userAuthReducer from '../features/authUserSlice';
 import authMessageReducer from '../features/authMessageSlice';
 import readersReducer from '../features/readerSlice';
+import bookSearchReducer from '../features/bookSearchSlice';
 
 export const store = configureStore({
   reducer: {
     userReaders: readersReducer,
+    bookSearchResults: bookSearchReducer,
     userAuth: userAuthReducer,
     authMessage: authMessageReducer,
   },
