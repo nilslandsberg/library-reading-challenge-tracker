bed614bd7b254d73229dc8c6b651e9df05cfbb8f
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-05-18 00:05:18 -0400

    delete book checkbox added to ReaderBooksTable. ReaderDetails Card size FIXED.

--- client/src/components/NonAuthView.js
+++ client/src/components/NonAuthView.js
@@ -1,6 +1,13 @@
 const NonAuthView = () => {
   return (
-    <h1>Not Logged In</h1>
+    <>
+    <h1>
+      Welcome to the 2023 Vienna Library Summer Reading Challenge!
+    </h1>
+    <h4>
+      Create an account for your household and track all of the reading done in your home this summer season.
+    </h4>
+    </>
   )
 };
 
