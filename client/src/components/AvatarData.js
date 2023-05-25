bd2e0cd61c590d3a1ed4bd489589fbf3c67085e4
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-05-24 23:08:14 -0400

    adult avatars added

--- client/src/components/AvatarData.js
+++ client/src/components/AvatarData.js
@@ -11,16 +11,29 @@ import pigTailGirl from '../files/avatars/4043252_child_girl_kid_person_icon.svg
 import zombie from '../files/avatars/4043266_avatar_dead_monster_zombie_icon.svg';
 import alien from '../files/avatars/4043268_alien_avatar_space_ufo_icon.svg';
 import harleyQuinn from '../files/avatars/4043270_avatar_joker_squad_suicide_woman_icon.svg';
+import baby from '../files/avatars/4043239_baby_child_kid_toddler_icon.svg';
+import woman from '../files/avatars/4043248_avatar_female_portrait_woman_icon.svg';
+import winkingWoman from '../files/avatars/4043251_avatar_female_girl_woman_icon.svg';
+import afroMan from '../files/avatars/4043279_afro_avatar_male_man_icon.svg';
+import hipster from '../files/avatars/4043255_beard_hipster_male_man_icon.svg';
+import heisenberg from '../files/avatars/4043240_avatar_bad_breaking_chemisrty_heisenberg_icon.svg';
+
 
   // avatars from 'https://www.iconfinder.com/iconsets/avatars-xmas-giveaway' pack by Laura Reen
   const avatars = [ 
+    baby,
     ponyTailGirl, 
     pigTailGirl, 
     hatBoy, 
     smilingBoy, 
-    spikedHairBoy, 
+    spikedHairBoy,
+    woman,
+    winkingWoman,
+    afroMan,
+    hipster,
     batman, 
-    harleyQuinn, 
+    harleyQuinn,
+    heisenberg, 
     screamingAvocado, 
     cactusPirate, 
     coffeeZorro, 
