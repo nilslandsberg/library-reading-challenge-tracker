539ebda306efb7265c2dccdba4b264a3156421d9
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2023-07-21 22:19:21 -0400

    User signup bug fixed.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -18,15 +18,18 @@ exports.signup = async (req, res) => {
 
     if (existingUsername) {
       return res.status(400).json({ message: "Username already in use" });
-    } else if (existingEmail) {
+    } 
+    
+    if (existingEmail) {
       return res.status(400).json({ message: "Account for email already exists" });
-    } else {
-      const newUser = await User.create({
-        username: username,
-        email: email,
-        password: password
-      });
     }
+    
+    const newUser = await User.create({
+      username: username,
+      email: email,
+      password: password
+    });
+  
     
     const token = createToken(newUser._id);
 
